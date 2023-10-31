2847b22f63b73fddcdac69be6d384227331b074e
Dev Gada <devgada@gmail.com>
2023-10-31 22:50:43 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 
-app.listen(3000, () => {
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to mongoDb')
+}).catch((err) => {
+    console.log(err)
+})
+   
+app.listen(3000, () => {   
     console.log('App listening on port 3000');
 })
-
+ 
